14813ac8f1e860d0d3e202781e44220f86bbff38
Iclipse <iclipse@gmx.de>
2019-04-19 11:25:12 +0200

    Just the first working Plugin

---
+++ src/de/Iclipse/PixelFruit/Main/Main.java
@@ -0,0 +1,8 @@
+package de.Iclipse.PixelFruit.Main;
+
+/**
+ * Created by Yannick who could get really angry if somebody steal his code!
+ * ~Yannick on 18.04.2019 at 10:36 o´ clock
+ */
+public class Main {
+}
